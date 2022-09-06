6c8bde03f443abe925dc18436ae3119580438a6a
ElanHashem <103283907+ElanHashem@users.noreply.github.com>
2022-09-06 13:42:04 -0700

    re

--- Poeltl.java
+++ Poeltl.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Poeltl
 {
-    private static final int NUM_DATABASE_PLAYERS = 100;
+    private static final int NUM_DATABASE_PLAYERS = 119;
     
 
     public static Player randomPlayer() throws FileNotFoundException
